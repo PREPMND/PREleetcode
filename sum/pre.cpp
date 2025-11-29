e0be652f7223098ba3fe30e4c9e1c5e14e7f230a
PREPMND <219679461+PREPMND@users.noreply.github.com>
2025-11-29 08:10:48 +0000

    Nov 29, 2025, 1:40 PM

--- sum/pre.cpp
+++ sum/pre.cpp
@@ -19,6 +19,6 @@ int main(){
         *ap='_';
         ap++;
     }
-    cout<<result[0]<<" "<<result[1]<<" "<<result[2]<<" "<<result[3]<<" "<<result[4]<<" "<<result[5]<<" "<<result[6]<<endl;
+    cout<<"["<<result[0]<<" , "<<result[1]<<" , "<<result[2]<<" , "<<result[3]<<" , "<<result[4]<<" , "<<result[5]<<" , "<<result[6]<<"]"<<endl;
     return 0;
 }
