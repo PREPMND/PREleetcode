260928bf6e98182de798baf9d481b0fc5001318b
PREPMND <219679461+PREPMND@users.noreply.github.com>
2025-12-26 21:34:19 +0530

    yyyes

--- PREques/loopton.cpp
+++ PREques/loopton.cpp
@@ -4,8 +4,22 @@ int main(){
     std::cout<<"Enter The Number You Want Your Lopp To Go Upto:";
     int n;
     std::cin>>n;
+    std::cout<<"Enter HOW Mnay Numbers You Want To skip:";
+    int skipCount;
+    std::cin>> skipCount;
+    int array[skipCount];
+    for(int i=0;i<skipCount;i++){
+        std::cout<<"Enter The "<< i+1 <<" Number You Want To skip:";
+        std::cin>>array[i];
+    }
     for (int i=0;i<n;i++){
-        std::cout<< i+1 <<std::endl;
+        bool skip;
+        for(int j=0;j<skipCount;j++){
+            if(i+1==array[j]){skip=true;i++;}
+        }
+        if(skip!=true){
+            std::cout<<i+1<<std::endl;
+        }
     }
     return 0;
 
---
+++ arrays/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int array[7]={1,3,4,5,6,7,2};
+    int largest=array[0];
+    for(int i=1;i<sizeof(array)/sizeof(array[0]);i++){
+        if(array[i]>largest){
+            largest=array[i];
+        }
+    }
+    cout<<"Largest element is: "<<largest<<endl;
+}
